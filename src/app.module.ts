a11a76b369f57b4db3406e6b4b31477b55163b26
chahanul <caroneul3014@gmail.com>
2025-04-17 17:20:50 +0900

    chore: user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,32 @@
+import { ConfigModule, ConfigService } from '@nestjs/config'
+
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { UsersModule } from './modules/users/users.module'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: +configService.get<number>('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true, // 개발 환경에서만 true로 설정
+      }),
+      inject: [ConfigService],
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
